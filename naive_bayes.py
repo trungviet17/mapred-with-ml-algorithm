0ce26ff8101cc1b3193874fb01b692fc2c7a7318
trungviet17 <theseventeengv@gmail.com>
2024-10-17 08:21:36 +0700

    update naivebayes algo

--- naive_bayes.py
+++ naive_bayes.py
@@ -11,63 +11,111 @@ class MRNaiveBayes(MRJob):
         2. Từ xác suất đã tính, nhận đầu vào là input mới chưa có nhãn 
             -> tính xác xuất từng trường xảy ra của từng trường hợp -> so sánh -> đưa ra nhãn 
     """
+    def configure_args(self): 
+        """
+        thêm đường dẫn tới file centroids vào cmd 
+        """
+        super(MRNaiveBayes, self).configure_args()
+        self.add_file_arg('--input_path', help = 'input file path ')
+
+    
+    def load_inut(self): 
+        
+        inputs = []
+        with open(self.options.input_path, 'r') as file: 
+            for line in file: 
+                line = line.strip().split(',')
+                inputs.append(
+                    (
+                        'outlook', line[0], 
+                        'temp', line[1], 
+                        'humidity', line[2], 
+                        'wind', line[3]
+                    )
+                )
+        return inputs
+
+    def mapper_init(self): 
+        self.inputs = self.load_inut()
+
+
+
     def mapper_train(self, _, line):
         line = line.strip().split(',') 
         target = line[-1] 
         label = ['outlook', 'temp', 'humidity', 'wind']
         for i in range(len(line) -1) : 
             yield (label[i], line[i], target) , 1
+        yield ('play', target), 1
 
 
     def reducer_train(self, key, value): 
         yield key, sum(value)
 
     
+    def mapper_infer(self, key, value):
+        
+        for i in range(len(self.inputs)): 
+            if key[0] == 'outlook': 
+                if key[1] == self.inputs[i][1]: 
+                    yield self.inputs[i], (key, value)
+            if key[0] == 'temp': 
+                if key[1] == self.inputs[i][3]: 
+                    yield self.inputs[i], (key, value)
+            if key[0] == 'humidity': 
+                if key[1] == self.inputs[i][5]: 
+                    yield self.inputs[i], (key, value)
+            if key[0] == 'wind': 
+                if key[1] == self.inputs[i][7]: 
+                    yield self.inputs[i], (key, value)
+            if key[0] == 'play': 
+                yield self.inputs[i], (key, value)
 
-    def init_mapper_infer(self):
+    def reducer_infer(self, key, values):
         """
-        prob : dùng để lưu xác xuất theo dạng p(featủe = value | target)
-        counts : lưu số lần xuất hiện target -> tính p(target)
+        Tính xác suất của từng trường hợp
         """
-        self.prob = defaultdict( lambda: defaultdict(lambda: defaultdict(int)))
-        self.counts = defaultdict(int) 
+        scores = defaultdict(float)
+        all_inst = 0 
+        for v in values: 
+            if len(v[0]) == 2: 
+                scores[v[0][-1]] = v[1]
+                all_inst += v[1]
 
+        scores_prob = {k: v/all_inst for k, v in scores.items()}
+        prob = defaultdict(float)
+        for v in values: 
+            if len(v[0]) == 3:
+                prob[v[0]] = v[1] / scores[v[0][-1]]
 
-    def mapper_infer(self, key, value): 
-        label, feature_value, target = key
-        self.counts[target] += value
-        self.prob[label][feature_value][target] += value
-    
+        # Ensure 'yes' and 'no' keys exist in scores_prob
+        for i in ['yes', 'no']: 
+            if i in scores_prob:
+                scores_prob[i] = math.log(scores_prob[i])
+            else:
+                scores_prob[i] = float('-inf')  # Assign a very low probability if the key is missing
 
-    def reducer_infer(self, _, line):
-        line = line.strip().split(',')
-        label = ['outlook', 'temp', 'humidity', 'wind']
-        targets = list(self.counts.keys())
+        for i in ['yes', 'no']: 
+            for k, v in prob.items(): 
+                if k[-1] == i:
+                    scores_prob[i] += math.log(v)
+        
+        yield key, max(scores_prob, key=scores_prob.get)
 
-        score = defaultdict(float)
-        for target in targets: 
-            score[target] = math.log(self.counts[target] / sum(self.counts.values()))
-
-            for i in range(len(line)): 
-                score[target] += math.log(self.prob[label[i]][line[i]][target] / self.counts[target])
-        result = max(score, key = score.get)
-        yield line, result
-
-    
 
 
     def steps(self): 
 
         return [
-            MRStep(mapper = self.mapper_train,
+            MRStep( mapper_init = self.mapper_init,
+                    mapper = self.mapper_train,
                     reducer = self.reducer_train), 
-            MRStep(mapper_init = self.init_mapper_infer,
+            MRStep( mapper_init = self.mapper_init,
                     mapper = self.mapper_infer,
-                    reducer = self.reducer_infer)
+                   reducer = self.reducer_infer)
         ]
 
 
 
 if __name__ == '__main__':
     MRNaiveBayes.run()
-    pass 
